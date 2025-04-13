612cd6962748c6241e9f53e97581b2abe6a9a222
bbaudry <baudry@kth.se>
2025-04-13 10:50:43 -0400

    gen in div

--- test-home/gen.js
+++ test-home/gen.js
@@ -2,16 +2,22 @@
 var cnv, w, h
 
 function setup(){
-    w=windowWidth
-    h=windowHeight
-    cnv = createCanvas(windowWidth, windowHeight);
+    var element = document.getElementById("left-side");
+    var positionInfo = element.getBoundingClientRect();
+    var divh = positionInfo.height;
+    var divw = positionInfo.width;
+    w=divw
+    h=divh
+    cnv = createCanvas(w, h);
     cnv.parent("left-side");
-    colorMode(HSB,360,100,100,250)
+    
+    colorMode(HSB, 360, 100, 100, 250);
 }
 
 function draw(){
     background(0,0,0)
     console.log("draw")
+    fill(0,0,100)
     vera()
     noLoop()
 }
@@ -19,12 +25,13 @@ function draw(){
 function vera(){
     var size,hsize,off,res,blanc
     res=Math.floor(random(2,5))
-    off=0//random(0.1,0.2)*w
+    off=random(0.01,0.05)*w
     blanc=true
     size = w * 0.9
     hsize = size * 0.5
+    noStroke()
     for(var i=0;i<res;i++){
-        if(blanc){fill(0, 100, 100);blanc=false}
+        if(blanc){fill(0, 0, 100);blanc=false}
         else{fill(0, 0, 0);blanc=true}
         quad(w * 0.5 - hsize + random(-off, off), h * 0.5 - hsize + random(-off, off),
             w * 0.5 + hsize + random(-off, off), h * 0.5 - hsize + random(-off, off),
