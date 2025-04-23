8d9b3b54039858beec8448cbd472a9e68a5854fe
bbaudry <baudry@kth.se>
2025-04-22 20:48:10 -0400

    generative interactive  404

--- test-home/404.js
+++ test-home/404.js
@@ -3,134 +3,243 @@ var cnv, w, h
 var leftmargin, topmargin, rightmargin, bottommargin, actualwidth, actualheight, pad
 
 function setup() {
-    w=windowWidth
-    h=windowHeight
+    w = windowWidth
+    h = windowHeight
     cnv = createCanvas(w, h);
     colorMode(HSB, 360, 100, 100, 250);
-    leftmargin = w*0.15
-    topmargin = h*0.05
-    rightmargin = w*0.85
-    bottommargin = h*0.95
-    pad = w*0.003
-    actualwidth = rightmargin-leftmargin
-    actualheight = bottommargin-topmargin
+    leftmargin = w * 0.15
+    topmargin = h * 0.05
+    rightmargin = w * 0.85
+    bottommargin = h * 0.95
+    pad = w * 0.003
+    actualwidth = rightmargin - leftmargin
+    actualheight = bottommargin - topmargin
 }
 
 /*404 in binary, needs 9 bits: 110 010 100*/
 function draw() {
-    background(0,0,0)
-    stroke(0,0,100)
-    fill(0,0,100)
-    toprightrows()
+    background(0, 0, 0)
+    stroke(0, 0, 100)
+    fill(0, 0, 100)
+    draw404()
     noLoop()
 }
 
-function topleftrows(){
-    var cellw,cellh
-    cellw=actualwidth/3
-    cellh=actualheight/3
+function draw404(){
+    background(0, 0, 0)
+    var dice = Math.floor(random(8))
+    switch (dice) {
+        case 0: topleftrows()
+            break;
+        case 1: topleftcolumns()
+            break;
+        case 2: toprightcolumns()
+            break;
+        case 3: toprightrows()
+            break;
+        case 4: bottomrightrows()
+            break;
+        case 5: bottomrightcolumns()
+            break;
+        case 6: bottomleftcolumns()
+            break;
+        case 7: bottomleftrows()
+            break;
 
-    line(leftmargin*0.5,topmargin,leftmargin,topmargin)
-
-    drawone(leftmargin,topmargin,cellw,cellh); 
-    drawone(leftmargin+cellw,topmargin,cellw,cellh); 
-    drawzero(leftmargin+2*cellw,topmargin,cellw,cellh);
-
-    drawzero(leftmargin,topmargin+cellh,cellw,cellh); 
-    drawone(leftmargin+cellw,topmargin+cellh,cellw,cellh); 
-    drawzero(leftmargin+2*cellw,topmargin+cellh,cellw,cellh);
-
-    drawone(leftmargin,topmargin+cellh*2,cellw,cellh); 
-    drawzero(leftmargin+cellw,topmargin+cellh*2,cellw,cellh); 
-    drawzero(leftmargin+2*cellw,topmargin+cellh*2,cellw,cellh);
+    }
 }
 
-function topleftcolumns(){
-    var cellw,cellh
-    cellw=actualwidth/3
-    cellh=actualheight/3
+function topleftrows() {
+    var cellw, cellh
+    cellw = actualwidth / 3
+    cellh = actualheight / 3
 
-    line(leftmargin,topmargin*0.5,leftmargin,topmargin)
+    line(leftmargin * 0.5, topmargin, leftmargin, topmargin)
 
-    drawone(leftmargin,topmargin,cellw,cellh); 
-    drawone(leftmargin,topmargin+cellh,cellw,cellh); 
-    drawzero(leftmargin,topmargin+cellh*2,cellw,cellh);
+    drawone(leftmargin, topmargin, cellw, cellh);
+    drawone(leftmargin + cellw, topmargin, cellw, cellh);
+    drawzero(leftmargin + 2 * cellw, topmargin, cellw, cellh);
 
-    drawzero(leftmargin+cellw,topmargin,cellw,cellh); 
-    drawone(leftmargin+cellw,topmargin+cellh,cellw,cellh); 
-    drawzero(leftmargin+cellw,topmargin+cellh*2,cellw,cellh);
+    drawzero(leftmargin, topmargin + cellh, cellw, cellh);
+    drawone(leftmargin + cellw, topmargin + cellh, cellw, cellh);
+    drawzero(leftmargin + 2 * cellw, topmargin + cellh, cellw, cellh);
 
-    drawone(leftmargin+2*cellw,topmargin,cellw,cellh); 
-    drawzero(leftmargin+2*cellw,topmargin+cellh,cellw,cellh); 
-    drawzero(leftmargin+2*cellw,topmargin+cellh*2,cellw,cellh);
+    drawone(leftmargin, topmargin + cellh * 2, cellw, cellh);
+    drawzero(leftmargin + cellw, topmargin + cellh * 2, cellw, cellh);
+    drawzero(leftmargin + 2 * cellw, topmargin + cellh * 2, cellw, cellh);
 }
 
-function toprightcolumns(){
-    var cellw,cellh
-    cellw=actualwidth/3
-    cellh=actualheight/3
+function topleftcolumns() {
+    var cellw, cellh
+    cellw = actualwidth / 3
+    cellh = actualheight / 3
 
-    line(rightmargin,topmargin*0.5,rightmargin,topmargin)
+    line(leftmargin, topmargin * 0.5, leftmargin, topmargin)
 
-    drawone(leftmargin+2*cellw,topmargin,cellw,cellh); 
-    drawone(leftmargin+2*cellw,topmargin+cellh,cellw,cellh); 
-    drawzero(leftmargin+2*cellw,topmargin+cellh*2,cellw,cellh);
+    drawone(leftmargin, topmargin, cellw, cellh);
+    drawone(leftmargin, topmargin + cellh, cellw, cellh);
+    drawzero(leftmargin, topmargin + cellh * 2, cellw, cellh);
 
-    drawzero(leftmargin+cellw,topmargin,cellw,cellh); 
-    drawone(leftmargin+cellw,topmargin+cellh,cellw,cellh); 
-    drawzero(leftmargin+cellw,topmargin+cellh*2,cellw,cellh);
+    drawzero(leftmargin + cellw, topmargin, cellw, cellh);
+    drawone(leftmargin + cellw, topmargin + cellh, cellw, cellh);
+    drawzero(leftmargin + cellw, topmargin + cellh * 2, cellw, cellh);
 
-    drawone(leftmargin,topmargin,cellw,cellh); 
-    drawzero(leftmargin,topmargin+cellh,cellw,cellh); 
-    drawzero(leftmargin,topmargin+cellh*2,cellw,cellh);
+    drawone(leftmargin + 2 * cellw, topmargin, cellw, cellh);
+    drawzero(leftmargin + 2 * cellw, topmargin + cellh, cellw, cellh);
+    drawzero(leftmargin + 2 * cellw, topmargin + cellh * 2, cellw, cellh);
 }
 
-function toprightrows(){
-    var cellw,cellh
-    cellw=actualwidth/3
-    cellh=actualheight/3
+function toprightcolumns() {
+    var cellw, cellh
+    cellw = actualwidth / 3
+    cellh = actualheight / 3
 
-    line(rightmargin,topmargin,rightmargin+leftmargin*0.5,topmargin)
+    line(rightmargin, topmargin * 0.5, rightmargin, topmargin)
 
-    drawone(leftmargin+2*cellw,topmargin,cellw,cellh); 
-    drawone(leftmargin+cellw,topmargin,cellw,cellh); 
-    drawzero(leftmargin,topmargin,cellw,cellh);
+    drawone(leftmargin + 2 * cellw, topmargin, cellw, cellh);
+    drawone(leftmargin + 2 * cellw, topmargin + cellh, cellw, cellh);
+    drawzero(leftmargin + 2 * cellw, topmargin + cellh * 2, cellw, cellh);
 
-    drawzero(leftmargin+2*cellw,topmargin+cellh,cellw,cellh); 
-    drawone(leftmargin+cellw,topmargin+cellh,cellw,cellh); 
-    drawzero(leftmargin,topmargin+cellh,cellw,cellh);
+    drawzero(leftmargin + cellw, topmargin, cellw, cellh);
+    drawone(leftmargin + cellw, topmargin + cellh, cellw, cellh);
+    drawzero(leftmargin + cellw, topmargin + cellh * 2, cellw, cellh);
 
-    drawone(leftmargin+2*cellw,topmargin+cellh*2,cellw,cellh); 
-    drawzero(leftmargin+cellw,topmargin+cellh*2,cellw,cellh); 
-    drawzero(leftmargin,topmargin+cellh*2,cellw,cellh);
+    drawone(leftmargin, topmargin, cellw, cellh);
+    drawzero(leftmargin, topmargin + cellh, cellw, cellh);
+    drawzero(leftmargin, topmargin + cellh * 2, cellw, cellh);
+}
+
+function toprightrows() {
+    var cellw, cellh
+    cellw = actualwidth / 3
+    cellh = actualheight / 3
+
+    line(rightmargin, topmargin, rightmargin + leftmargin * 0.5, topmargin)
+
+    drawone(leftmargin + 2 * cellw, topmargin, cellw, cellh);
+    drawone(leftmargin + cellw, topmargin, cellw, cellh);
+    drawzero(leftmargin, topmargin, cellw, cellh);
+
+    drawzero(leftmargin + 2 * cellw, topmargin + cellh, cellw, cellh);
+    drawone(leftmargin + cellw, topmargin + cellh, cellw, cellh);
+    drawzero(leftmargin, topmargin + cellh, cellw, cellh);
+
+    drawone(leftmargin + 2 * cellw, topmargin + cellh * 2, cellw, cellh);
+    drawzero(leftmargin + cellw, topmargin + cellh * 2, cellw, cellh);
+    drawzero(leftmargin, topmargin + cellh * 2, cellw, cellh);
+}
+
+function bottomrightrows() {
+    var cellw, cellh
+    cellw = actualwidth / 3
+    cellh = actualheight / 3
+
+    line(rightmargin, bottommargin, rightmargin + leftmargin * 0.5, bottommargin)
+
+    drawone(leftmargin + 2 * cellw, topmargin + cellh * 2, cellw, cellh);
+    drawone(leftmargin + cellw, topmargin + cellh * 2, cellw, cellh);
+    drawzero(leftmargin, topmargin + cellh * 2, cellw, cellh);
+
+    drawzero(leftmargin + 2 * cellw, topmargin + cellh, cellw, cellh);
+    drawone(leftmargin + cellw, topmargin + cellh, cellw, cellh);
+    drawzero(leftmargin, topmargin + cellh, cellw, cellh);
+
+    drawone(leftmargin + 2 * cellw, topmargin, cellw, cellh);
+    drawzero(leftmargin + cellw, topmargin, cellw, cellh);
+    drawzero(leftmargin, topmargin, cellw, cellh);
+}
+
+function bottomrightcolumns() {
+    var cellw, cellh
+    cellw = actualwidth / 3
+    cellh = actualheight / 3
+
+    line(rightmargin, bottommargin + topmargin * 0.5, rightmargin, bottommargin)
+
+    drawone(leftmargin + 2 * cellw, topmargin + cellh * 2, cellw, cellh);
+    drawone(leftmargin + 2 * cellw, topmargin + cellh, cellw, cellh);
+    drawzero(leftmargin + 2 * cellw, topmargin, cellw, cellh);
+
+    drawzero(leftmargin + cellw, topmargin + cellh * 2, cellw, cellh);
+    drawone(leftmargin + cellw, topmargin + cellh, cellw, cellh);
+    drawzero(leftmargin + cellw, topmargin, cellw, cellh);
+
+    drawone(leftmargin, topmargin + cellh * 2, cellw, cellh);
+    drawzero(leftmargin, topmargin + cellh, cellw, cellh);
+    drawzero(leftmargin, topmargin, cellw, cellh);
+}
+
+function bottomleftcolumns() {
+    var cellw, cellh
+    cellw = actualwidth / 3
+    cellh = actualheight / 3
+
+    line(leftmargin, bottommargin + topmargin * 0.5, leftmargin, bottommargin)
+
+    drawone(leftmargin, topmargin + cellh * 2, cellw, cellh);
+    drawone(leftmargin, topmargin + cellh, cellw, cellh);
+    drawzero(leftmargin, topmargin, cellw, cellh);
+
+    drawzero(leftmargin + cellw, topmargin + cellh * 2, cellw, cellh);
+    drawone(leftmargin + cellw, topmargin + cellh, cellw, cellh);
+    drawzero(leftmargin + cellw, topmargin, cellw, cellh);
+
+    drawone(leftmargin + 2 * cellw, topmargin + cellh * 2, cellw, cellh);
+    drawzero(leftmargin + 2 * cellw, topmargin + cellh, cellw, cellh);
+    drawzero(leftmargin + 2 * cellw, topmargin, cellw, cellh);
+}
+
+function bottomleftrows() {
+    var cellw, cellh
+    cellw = actualwidth / 3
+    cellh = actualheight / 3
+
+    line(leftmargin * 0.5, bottommargin, leftmargin, bottommargin)
+
+    drawone(leftmargin, topmargin + cellh * 2, cellw, cellh);
+    drawone(leftmargin + cellw, topmargin + cellh * 2, cellw, cellh);
+    drawzero(leftmargin + 2 * cellw, topmargin + cellh * 2, cellw, cellh);
+
+    drawzero(leftmargin, topmargin + cellh, cellw, cellh);
+    drawone(leftmargin + cellw, topmargin + cellh, cellw, cellh);
+    drawzero(leftmargin + 2 * cellw, topmargin + cellh, cellw, cellh);
+
+    drawone(leftmargin, topmargin, cellw, cellh);
+    drawzero(leftmargin + cellw, topmargin, cellw, cellh);
+    drawzero(leftmargin + 2 * cellw, topmargin, cellw, cellh);
 }
 
 
-function drawone(x,y,cellw,cellh){
+
+function drawone(x, y, cellw, cellh) {
     push()
-    cellw=cellw-pad*2
-    cellh=cellh-pad*2
-    translate(x,y)
-    rect(pad+cellw*0.4,pad,cellw*0.2,cellh)
+    cellw = cellw - pad * 2
+    cellh = cellh - pad * 2
+    translate(x, y)
+    rect(pad + cellw * 0.4, pad, cellw * 0.2, cellh)
     pop()
 }
 
-function drawzero(x,y,cellw,cellh){
+function drawzero(x, y, cellw, cellh) {
     push()
-    cellw=cellw-pad*2
-    cellh=cellh-pad*2
-    translate(x,y)
-    rect(pad,pad,cellw,cellh*0.2)
-    rect(pad,pad+cellh*0.2,cellw*0.2,cellh*0.8)
-    rect(pad+cellw*0.8,pad+cellh*0.2,cellw*0.2,cellh*0.8)
-    rect(pad,pad+cellh*0.8,cellw,cellh*0.2)
+    cellw = cellw - pad * 2
+    cellh = cellh - pad * 2
+    translate(x, y)
+    rect(pad, pad, cellw, cellh * 0.2)
+    rect(pad, pad + cellh * 0.2, cellw * 0.2, cellh * 0.8)
+    rect(pad + cellw * 0.8, pad + cellh * 0.2, cellw * 0.2, cellh * 0.8)
+    rect(pad, pad + cellh * 0.8, cellw, cellh * 0.2)
     pop()
 }
 
 function windowResized() {
-    w=windowWidth
-    h=windowHeight
+    w = windowWidth
+    h = windowHeight
     resizeCanvas(w, h);
     setCanvasSize()
 }
 
+
+function mouseMoved() {
+    draw404()
+}
