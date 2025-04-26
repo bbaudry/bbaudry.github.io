0a530eb4a27d953de956f0628c8ad20593eed91e
bbaudry <baudry@kth.se>
2025-04-26 13:23:20 -0400

    experiment 404

--- test-home/404b.js
+++ test-home/404b.js
@@ -2,43 +2,69 @@
 var cnv, w, h
 var leftmargin, topmargin, rightmargin, bottommargin, actualwidth, actualheight, pad
 var font, fSize
+var pixs = []
 
 
-function preload(){
-    font = loadFont("ChunkFive-Regular.otf")
+function preload() {
+  font = loadFont("ChunkFive-Regular.otf")
 }
 function setup() {
-    w = windowWidth
-    h = windowHeight
-    cnv = createCanvas(w, h);
-    colorMode(HSB, 360, 100, 100, 250);
-    leftmargin = w * 0.15
-    topmargin = h * 0.05
-    rightmargin = w * 0.85
-    bottommargin = h * 0.95
-    pad = w * 0.003
-    actualwidth = rightmargin - leftmargin
-    actualheight = bottommargin - topmargin
+  w = windowWidth
+  h = windowHeight
+  cnv = createCanvas(w, h);
+  colorMode(HSB, 360, 100, 100, 250);
+  leftmargin = w * 0.15
+  topmargin = h * 0.05
+  rightmargin = w * 0.85
+  bottommargin = h * 0.95
+  pad = w * 0.003
+  actualwidth = rightmargin - leftmargin
+  actualheight = bottommargin - topmargin
+  stroke(0, 0, 100)
+  fill(0, 0, 100)
+  background(0, 0, 0)
+  textFont(font)
+  textSize(actualheight * 0.8)
+  var msg = "404"
+  var x = leftmargin + (actualwidth - textWidth(msg)) * 0.5
+  //text("404",x,bottommargin*0.8)
+  // Get the point array.
+  let points = font.textToPoints("404", x, bottommargin * 0.8, actualheight * 0.8, { sampleFactor: 0.02 });
+
+  // Draw a dot at each point.
+  for (let p of points) {
+    //rect(p.x, p.y, actualwidth * 0.029, actualwidth * 0.029)
+    var pix = new Pix(p.x, p.y, actualwidth * 0.029, actualwidth * 0.029)
+    pixs.push(pix)
+
+  }
+  fill(110, 100, 80)
+  noStroke()
 }
 
 /*404 in binary, needs 9 bits: 110 010 100*/
 function draw() {
-    background(0, 0, 0)
-    stroke(0, 0, 100)
-    fill(0, 0, 100)
-    textFont(font)
-    textSize(actualheight*0.8)
-    var msg="404"
-    var x=leftmargin+(actualwidth-textWidth(msg))*0.5
-    //text("404",x,bottommargin*0.8)
-      // Get the point array.
-  let points = font.textToPoints("404",x,bottommargin*0.8, actualheight*0.8,{ sampleFactor:  0.02 });
+  for (i in pixs) {
+    pixs[i].show()
+  }
+}
 
-  // Draw a dot at each point.
-  for (let p of points) {
-    //ellipse(p.x,p.y,5,5)
-    rect(p.x,p.y,actualwidth*0.029,actualwidth*0.029)
+class Pix {
+  constructor(x, y, w, h) {
+    this.x = x
+    this.y = y
+    this.w = w
+    this.h = h
+    this.r = w*0.3
   }
 
-    noLoop()
+  show() {
+    var lx, ly
+    lx = mouseX
+    ly = mouseY
+    if (lx > this.x && lx < this.x + this.w && ly > this.y && ly < this.y + this.h) {
+      rect(lx, ly,this.r,this.r)
+      console.log("paint at x: "+lx+", y: "+ly)
+    }
+  }
 }
