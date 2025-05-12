7ab3fb49809d8f5e9c9e8146b72b931f0e3101f8
bbaudry <baudry@kth.se>
2025-05-12 17:51:42 -0400

    cleaning bib

--- test-home/bibsplit/bibsplit.py
+++ test-home/bibsplit/bibsplit.py
@@ -1,35 +1,19 @@
 import bibtexparser
 
-bibkeys = []
 
-bibtex_str = """
-@comment{
-    This is my example comment.
-}
-
-@ARTICLE{Cesar2013,
-  author = {Jean César},
-  title = {An amazing title},
-  year = {2013},
-  volume = {12},
-  pages = {12--23},
-  journal = {Nice Journal}
-}
-"""
-print(dir(bibtexparser))
+articles = bibtexparser.Library()
 library=bibtexparser.parse_file("publis.bib")
-for b in library.blocks:
-    b.ignore_error_block()
 print(f"Parsed {len(library.blocks)} blocks, including:"
   f"\n\t{len(library.entries)} entries"
     f"\n\t{len(library.comments)} comments"
     f"\n\t{len(library.strings)} strings and"
     f"\n\t{len(library.preambles)} preambles")
 for entry in library.entries:
-    if (entry.key in bibkeys):
-        print(entry.key)
-    else:
-        bibkeys.append(entry.key)
+    if(entry.entry_type == "article"):
+            articles.set_field(entry)
+print(len(articles))
+new_bibtex_str = bibtexparser.write_string(entry) # or bibtexparser.write_file("my_new_file.bib", library)
+#print(new_bibtex_str)
 #    print(entry["author"])
 #    if ('keywords' in entry.fields_dict.keys()):
 #        print(entry["keywords"])
