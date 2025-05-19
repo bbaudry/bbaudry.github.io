4c4a0cc50f0604f93987dc398b1fba73f6f5c46e
bbaudry <baudry@kth.se>
2025-05-19 17:51:17 -0400

    more content

--- test-home/regenerate.sh
+++ test-home/regenerate.sh
@@ -1,15 +1,33 @@
 #! /usr/bin/bash
 
-bibtool -- 'select{@article}' publis.bib -o journalpublications.bib
-pandoc journalpublications.bib -s --template=publications-template.html --metadata link-bibliography --metadata title="Journal publications" --csl ieee.csl --citeproc -o journalpublications.html
+filename="journalpublications"
+filebib=$filename".bib"
+filehtm=$filename".html"
+bibtool -- 'select{@article}' publis.bib -o $filebib
+pandoc $filebib -s --template=publications-template.html --metadata link-bibliography --metadata title="Journal publications" --csl ieee.csl --citeproc -o $filehtm
 
-bibtool -- 'select{@inproceedings}' publis.bib -o conferencepublications.bib
-pandoc conferencepublications.bib -s --template=publications-template.html --metadata link-bibliography --metadata title="Conference publications" --csl ieee.csl --citeproc -o conferencepublications.html
+filename="conferencepublications"
+filebib=$filename".bib"
+filehtm=$filename".html"
+bibtool -- 'select{@inproceedings}' publis.bib -o $filebib
+pandoc $filebib -s --template=publications-template.html --metadata link-bibliography --metadata title="Conference publications" --csl ieee.csl --citeproc -o $filehtm
 
-bibtool -- 'select{keywords "selected"}' publis.bib -o selectedpublications.bib
-pandoc selectedpublications.bib -s --template=publications-template.html --metadata link-bibliography --metadata title="Selected publications" --csl ieee.csl --citeproc -o selectedpublications.html
+filename="selectedpublications"
+filebib=$filename".bib"
+filehtm=$filename".html"
+bibtool -- 'select{keywords "selected"}' publis.bib -o $filebib
+pandoc $filebib -s --template=publications-template.html --metadata link-bibliography --metadata title="Selected publications" --csl ieee.csl --citeproc -o $filehtm
 
-bibtool -- 'select{keywords "test"}' publis.bib -o testpublications.bib
-pandoc selectedpublications.bib -s --template=publications-template.html --metadata link-bibliography --metadata title="Testing publications" --csl ieee.csl --citeproc -o testpublications.html
+filename="testpublications"
+filebib=$filename".bib"
+filehtm=$filename".html"
+bibtool -- 'select{keywords "test"}' publis.bib -o $filebib
+pandoc $filebib -s --template=publications-template.html --metadata link-bibliography --metadata title="Testing publications" --csl ieee.csl --citeproc -o $filehtm
+
+filename="diversitypublications"
+filebib=$filename".bib"
+filehtm=$filename".html"
+bibtool -- 'select{keywords "diversity"}' publis.bib -o $filebib
+pandoc $filebib -s --template=publications-template.html --metadata link-bibliography --metadata title="Diversity publications" --csl ieee.csl --citeproc -o $filehtm
 
 pandoc -s --toc=true  --template=topics-template.html  topics.md -o topics.html
