58abafaed3e9d1f6c4b850f9a3adf9f1f3fc363a
bbaudry <baudry@kth.se>
2025-05-19 17:40:29 -0400

    refactor gen biblio

--- test-home/regenerate.sh
+++ test-home/regenerate.sh
@@ -1,15 +1,15 @@
 #! /usr/bin/bash
 
 bibtool -- 'select{@article}' publis.bib -o journalpublications.bib
-pandoc journalpublications.bib -s --template=journalpublications-template.html --metadata link-bibliography --csl ieee.csl --citeproc -o journalpublications.html
+pandoc journalpublications.bib -s --template=publications-template.html --metadata link-bibliography --metadata title="Journal publications" --csl ieee.csl --citeproc -o journalpublications.html
 
 bibtool -- 'select{@inproceedings}' publis.bib -o conferencepublications.bib
-pandoc conferencepublications.bib -s --template=conferencepublications-template.html --metadata link-bibliography --csl ieee.csl --citeproc -o conferencepublications.html
+pandoc conferencepublications.bib -s --template=publications-template.html --metadata link-bibliography --metadata title="Conference publications" --csl ieee.csl --citeproc -o conferencepublications.html
 
 bibtool -- 'select{keywords "selected"}' publis.bib -o selectedpublications.bib
-pandoc selectedpublications.bib -s --template=selectedpublications-template.html --metadata link-bibliography --csl ieee.csl --citeproc -o selectedpublications.html
+pandoc selectedpublications.bib -s --template=publications-template.html --metadata link-bibliography --metadata title="Selected publications" --csl ieee.csl --citeproc -o selectedpublications.html
 
 bibtool -- 'select{keywords "test"}' publis.bib -o testpublications.bib
-pandoc selectedpublications.bib -s --template=testpublications-template.html --metadata link-bibliography --csl ieee.csl --citeproc -o testpublications.html
+pandoc selectedpublications.bib -s --template=publications-template.html --metadata link-bibliography --metadata title="Testing publications" --csl ieee.csl --citeproc -o testpublications.html
 
 pandoc -s --toc=true  --template=topics-template.html  topics.md -o topics.html
